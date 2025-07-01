044b6f30bef2ec0b40c5ff15ccbc1cfdc6f90b09
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-07-01 11:33:45 +0900

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,29 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+buildscript {
+    val kotlinVersion by extra("1.8.21")
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+    group = "com.github.tjlabs"
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
