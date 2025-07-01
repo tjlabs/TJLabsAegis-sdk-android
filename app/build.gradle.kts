6aef65e4aa3e80d3baaae2a077a8b5ce59d0ac4f
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-07-01 11:26:06 +0900

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,29 @@
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import java.util.Properties
+
 plugins {
-    id("com.android.application")
-//    id("com.android.library")
+//    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
+
+val versionMajor = 1
+val versionMinor = 0
+val versionPatch = 0
+
+// 최상단에 import 추가
+
+// local.properties 파일 읽기
+val localProperties = Properties().apply {
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        load(localPropertiesFile.inputStream())
+    }
+}
+
+val tjGroupId: String = localProperties.getProperty("GROUP_ID") ?: ""
+val tjArtifactId: String = localProperties.getProperty("ARTIFACT_ID") ?: ""
 
 
 android {
@@ -11,13 +31,20 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.tjlabs.tjlabsaegis_sdk_android"
+//        applicationId = "com.tjlabs.tjlabsaegis_sdk_android"
+//        versionCode = versionMajor * 1000 + versionMinor * 100 + versionPatch
+//        versionName = "$versionMajor.$versionMinor.$versionPatch"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "TJLABS_SDK_VERSION", "\"$versionMajor.$versionMinor.$versionPatch\"")
+
+    }
+
+    libraryVariants.all {
+        outputs.all {
+            (this as BaseVariantOutputImpl).outputFileName = "app-release-aegis.aar"
+        }
     }
 
     buildTypes {
@@ -55,7 +82,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = tjGroupId
+                artifactId = tjArtifactId
+                version = "$versionMajor.$versionMinor.$versionPatch"
+            }
+        }
+    }
 }
